86a289b120352b69faf9e3c2f2c494a3a5ae1edb
Max <contact@maxsnow.me>
2018-06-01 17:02:08 +1000

    Add check role middleware

---
+++ src/Http/Middleware/CheckUserRole.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Specialtactics\L5Api\Http\Middleware;
+
+use Closure;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+
+class CheckUserRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param array|string $roles Roles to check for
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        $loggedInUser = $request->user();
+
+        // Check user is logged in
+        if (empty($loggedInUser)) {
+            throw new UnauthorizedHttpException('Unauthorized - not logged in!');
+        }
+
+        if (!is_array($roles)) {
+            $roles = [$roles];
+        }
+
+        // Match user roles to requested roles
+        $matchedRoles = array_intersect($roles, $loggedInUser->getRoles());
+
+        if (empty($matchedRoles)) {
+            throw new UnauthorizedHttpException('You do not have the permission to use this resource');
+        }
+
+        return $next($request);
+    }
+}

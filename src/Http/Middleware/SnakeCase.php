76f7e80a8bbe6d9d637ab007181199ba468030d2
Max <contact@maxsnow.me>
2018-09-26 15:05:41 +1000

    SnakeCase middleware vastly improved (does json, request and query parameter bags now), more in line with how laravel works as opposed to symphony

--- src/Http/Middleware/SnakeCase.php
+++ src/Http/Middleware/SnakeCase.php
@@ -3,15 +3,31 @@
 namespace Specialtactics\L5Api\Http\Middleware;
 
 use Closure;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
-class SnakeCase
+/**
+ * Class SnakeCaseInputParameterKeys
+ *
+ * This middleware makes sure all incoming request parameters are snake cased for the application
+ *
+ * @package Specialtactics\L5Api\Http\Middleware
+ */
+class SnakeCaseInputParameterKeys
 {
-    protected const RELEVANT_METHODS = ['POST', 'PATCH', 'PUT', 'DELETE', 'GET'];
+    /**
+     * HTTP Methods we want to consider for transforming URL query params
+     */
+    protected const RELEVANT_METHODS_QUERY = ['POST', 'PATCH', 'PUT', 'DELETE', 'GET'];
+
+    /**
+     * HTTP methods we want to consider for transorming request body input
+     */
+    protected const RELEVANT_METHODS_BODY = ['POST', 'PATCH', 'PUT', 'DELETE'];
 
     /**
      * Handle an incoming request.
      *
-     * Replace all request keys with snake_cased equivilents
+     * Replace all request parameter keys with snake_cased equivilents
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
@@ -20,17 +36,35 @@ class SnakeCase
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (in_array($request->method(), static::RELEVANT_METHODS)) {
+        // Query string parameters
+        if (in_array($request->method(), static::RELEVANT_METHODS_QUERY)) {
+            $this->processParamBag($request->query);
+        }
 
-            $parameters = $request->request->all();
+        // Input parameters
+        if (in_array($request->method(), static::RELEVANT_METHODS_BODY)) {
+            $this->processParamBag($request->request);
 
-            if (!empty($parameters) && count($parameters) > 0) {
-                $parameters = snake_case_array_keys($parameters);
-
-                $request->request->replace($parameters);
+            if ($request->isJson()) {
+                $this->processParamBag($request->json());
             }
         }
 
         return $next($request);
+    }
+
+    /**
+     * Process parameters within a ParameterBag to snake_case the keys
+     *
+     * @param ParameterBag $bag
+     */
+    protected function processParamBag(ParameterBag $bag) {
+        $parameters = $bag->all();
+
+        if (!empty($parameters) && count($parameters) > 0) {
+            $parameters = snake_case_array_keys($parameters);
+
+            $bag->replace($parameters);
+        }
     }
 }

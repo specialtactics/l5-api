1d30e1f6a66a5cced28debbebcbfa813fadd511f
Max <contact@maxsnow.me>
2018-10-21 23:27:07 +1100

    These changes allow the formatting of case for responses to be configured in the api config and even set during request using http headers

---
+++ src/APIBoilerPlate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Specialtactics\L5Api;
+
+use Config;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class APIBoilerPlate {
+    /**
+     * Case type constants for configuring responses
+     */
+    const CAMEL_CASE = 'camel-case';
+    const SNAKE_CASE = 'snake-case';
+
+    /**
+     * The header which can be used to override config provided case type
+     */
+    const CASE_TYPE_HEADER = 'X-Accept-Case-Type';
+
+    /**
+     * Get the required 'case type' for transforming response data
+     *
+     * @return string
+     */
+    public static function getResponseCaseType() {
+        $format = null;
+
+        // See if the client is requesting a specific case type
+        $caseFormat = request()->header(static::CASE_TYPE_HEADER, null);
+        if (!is_null($caseFormat)) {
+            if ($caseFormat == static::CAMEL_CASE) {
+                $format = static::CAMEL_CASE;
+            } else if ($caseFormat == static::SNAKE_CASE) {
+                $format = static::SNAKE_CASE;
+            }
+        }
+
+        // Get case format from config (default case)
+        if (is_null($format)) {
+            $caseFormat = Config('api.formatsOptions.caseType');
+
+            // Figure out required case
+            if ($caseFormat == static::CAMEL_CASE || empty($caseFormat)) {
+                $format = static::CAMEL_CASE;
+            } else if ($caseFormat == static::SNAKE_CASE) {
+                $format = static::SNAKE_CASE;
+            } else {
+                throw new HttpException(500, 'Invalid case type specified in API config.');
+            }
+        }
+
+        return $format;
+    }
+}

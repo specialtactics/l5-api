120daeeeee7e2e29a177c262b9049c8533584d4b
Max <contact@maxsnow.me>
2019-09-27 13:51:21 +1000

    Replace str_ helper

--- test/database/factories/UserFactory.php
+++ test/database/factories/UserFactory.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Str;
 
 use Faker\Generator as Faker;
 
@@ -18,6 +19,6 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });

6d7d1b5ca09d6369a0529c7eb507792ab138412b
Max <contact@maxsnow.me>
2019-10-08 11:39:00 +1100

    Commit a test to run first and eat up bootstrap time

---
+++ test/tests/A/FirstTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Specialtactics\L5Api\Tests\A;
+
+use Specialtactics\L5Api\Tests\BaseTestCase;
+
+class FirstTest extends BaseTestCase
+{
+    /**
+     * @testdox Consumes testing bootstrap time
+     * @test
+     */
+    public function testBootstrapTime()
+    {
+        $this->assertTrue(true);
+    }
+}

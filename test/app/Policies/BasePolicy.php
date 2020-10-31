0518057b01a6931b16530a59413f642658ff0f9b
Max <contact@maxsnow.me>
2020-10-31 22:53:18 +1100

    Laravel 8 support - composer updates

--- test/app/Policies/BasePolicy.php
+++ test/app/Policies/BasePolicy.php
@@ -3,7 +3,21 @@
 namespace App\Policies;
 
 use Specialtactics\L5Api\Policies\RestfulModelPolicy;
+use App\Models\User;
 
 class BasePolicy extends RestfulModelPolicy
 {
+    /**
+     * Process 'global' authorisation rules
+     *
+     * @param $user
+     * @param $ability
+     * @return bool
+     */
+    public function before(User $user, $ability)
+    {
+        if ($user->isAdmin()) {
+            return true;
+        }
+    }
 }
